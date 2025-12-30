8b9e621bc647fcf9156da199d1a519a8129d7a1a
Gafiew <fiewture.z@gmail.com>
2025-12-30 11:49:32 +0700

    Update API rewrites for production

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,12 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:3000/api/:path*', // Backend Port (เช็คให้ชัวร์ว่า 3000)
-      },
-      // 👇 เพิ่มก้อนนี้เข้าไปครับ
-      {
-        source: '/socket.io/:path*',
-        destination: 'http://localhost:3000/socket.io/:path*', // ส่งต่อ Socket ไปหา Backend
+        destination: process.env.NEXT_PUBLIC_API_URL 
+          ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*` 
+          : 'http://localhost:3000/api/:path*',
       },
     ]
   },
 };
+
 export default nextConfig;
